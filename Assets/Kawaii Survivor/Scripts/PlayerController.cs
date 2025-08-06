c9ed7a67aabffc56bf4f20d21e02856d75f13358
Elder Luiz <elderzxvm99@gmail.com>
2025-08-06 17:59:58 +0900

    initial commit

---
+++ Assets/Kawaii Survivor/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D Rigidbody2D;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
