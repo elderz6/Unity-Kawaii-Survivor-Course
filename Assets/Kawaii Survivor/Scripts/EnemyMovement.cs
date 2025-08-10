bb623dea00e39d24174881648ffb6538335670fa
Elder Luiz <elderzxvm99@gmail.com>
2025-08-10 17:00:24 +0900

    Added enemy and movement

---
+++ Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("Elements")] 
+    private Player player;
+
+    [Header("Settings")] 
+    [SerializeField] float moveSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = FindFirstObjectByType<Player>();
+        
+        if(player == null)
+            Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = (player.transform.position - this.transform.position).normalized;
+        Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
+        transform.position = targetPosition;
+    }
+    
+    
+}
