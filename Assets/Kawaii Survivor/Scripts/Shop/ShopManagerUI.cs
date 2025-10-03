388789bc0a787a66319b27b57b98f73a267f5e2f
Elder Luiz <elderzxvm99@gmail.com>
2025-10-03 13:32:54 +0900

    inventory panel added

--- Assets/Kawaii Survivor/Scripts/Shop/ShopManagerUI.cs
+++ Assets/Kawaii Survivor/Scripts/Shop/ShopManagerUI.cs
@@ -4,17 +4,23 @@ using UnityEngine.UI;
 
 public class ShopManagerUI : MonoBehaviour
 {
-    [Header("Elements")]
+    [Header("Stats Elements")]
     [SerializeField] private RectTransform playerStatsPanel;
     [SerializeField] private GameObject playerStatsBackground;
-
     private Vector2 playerStatsOpenPos;
     private Vector2 playerStatsClosedPos;
+    
+    [Header("Inventory Elements")]
+    [SerializeField] private RectTransform inventoryPanel;
+    [SerializeField] private GameObject inventoryBackground;
+    private Vector2 inventoryOpenPos;
+    private Vector2 inventoryClosedPos;
 
     IEnumerator Start()
     {
         yield return null;
         ConfigureStatsPanel();
+        ConfigureInventoryPanel();
     }
 
     private void ConfigureStatsPanel()
@@ -48,5 +54,38 @@ public class ShopManagerUI : MonoBehaviour
         LeanTween.move(playerStatsPanel, playerStatsClosedPos, .3f)
             .setEase(LeanTweenType.easeOutCubic)
             .setOnComplete(() => playerStatsPanel.gameObject.SetActive(false));
+    }
+
+    private void ConfigureInventoryPanel()
+    {
+        float width = Screen.width / (4 * inventoryPanel.lossyScale.x);
+        inventoryPanel.offsetMin = inventoryPanel.offsetMin.With(x: -width);
+
+        inventoryOpenPos = inventoryPanel.anchoredPosition;
+        inventoryClosedPos = inventoryOpenPos - Vector2.left * width;
+
+        inventoryPanel.anchoredPosition = inventoryClosedPos;
+        HideInventory();
+    }
+
+    public void HideInventory()
+    {
+        inventoryBackground.SetActive(false);
+        inventoryBackground.GetComponent<Image>().raycastTarget = false;
+        
+        LeanTween.cancel(inventoryPanel);
+        LeanTween.move(inventoryPanel, inventoryClosedPos, .3f)
+            .setEase(LeanTweenType.easeOutCubic)
+            .setOnComplete(() => inventoryPanel.gameObject.SetActive(false));
+    }
+
+    public void ShowInventory()
+    {
+        inventoryPanel.gameObject.SetActive(true);
+        inventoryBackground.SetActive(true);
+        inventoryBackground.GetComponent<Image>().raycastTarget = true;
+
+        LeanTween.cancel(inventoryPanel);
+        LeanTween.move(inventoryPanel, inventoryOpenPos, .3f).setEase(LeanTweenType.easeInCubic);
     }
 }
