bde8ef106b4c5f14114b44276693e40e6e0eda6a
Elder Luiz <elderzxvm99@gmail.com>
2025-10-11 19:40:05 +0900

    inventory double sliding panel

--- Assets/Kawaii Survivor/Scripts/Shop/ShopManagerUI.cs
+++ Assets/Kawaii Survivor/Scripts/Shop/ShopManagerUI.cs
@@ -15,12 +15,46 @@ public class ShopManagerUI : MonoBehaviour
     [SerializeField] private GameObject inventoryBackground;
     private Vector2 inventoryOpenPos;
     private Vector2 inventoryClosedPos;
+    
+    [Header("Item Info Elements")]
+    [SerializeField] private RectTransform itemInfoPanel; 
+    private Vector2 itemInfoOpenPos;
+    private Vector2 itemInfoClosedPos;
 
     IEnumerator Start()
     {
         yield return null;
         ConfigureStatsPanel();
         ConfigureInventoryPanel();
+        ConfigureItemInfoPanel();
+    }
+
+    private void ConfigureItemInfoPanel()
+    {
+        float height = Screen.height / (2 * itemInfoPanel.lossyScale.x);
+        itemInfoPanel.offsetMax = itemInfoPanel.offsetMax.With(y: height);
+
+        itemInfoOpenPos = itemInfoPanel.anchoredPosition;
+        itemInfoClosedPos = itemInfoOpenPos + Vector2.down * height;
+        
+        itemInfoPanel.anchoredPosition = itemInfoClosedPos;
+        itemInfoPanel.gameObject.SetActive(false);
+    }
+
+    [NaughtyAttributes.Button]
+    public void ShowItemInfo()
+    {
+        itemInfoPanel.gameObject.SetActive(true);
+        itemInfoPanel.LeanCancel();
+        itemInfoPanel.LeanMove((Vector3)itemInfoOpenPos, .3f).setEase(LeanTweenType.easeOutCubic);
+    }
+
+    [NaughtyAttributes.Button]
+    public void HideItemInfo()
+    {
+        itemInfoPanel.LeanCancel();
+        itemInfoPanel.LeanMove((Vector3)itemInfoClosedPos, .3f).setEase(LeanTweenType.easeInCubic)
+            .setOnComplete(() => itemInfoPanel.gameObject.SetActive(false));
     }
 
     private void ConfigureStatsPanel()
@@ -65,10 +99,10 @@ public class ShopManagerUI : MonoBehaviour
         inventoryClosedPos = inventoryOpenPos - Vector2.left * width;
 
         inventoryPanel.anchoredPosition = inventoryClosedPos;
-        HideInventory();
+        HideInventory(false);
     }
 
-    public void HideInventory()
+    public void HideInventory(bool hideItem)
     {
         inventoryBackground.SetActive(false);
         inventoryBackground.GetComponent<Image>().raycastTarget = false;
@@ -77,6 +111,8 @@ public class ShopManagerUI : MonoBehaviour
         LeanTween.move(inventoryPanel, inventoryClosedPos, .3f)
             .setEase(LeanTweenType.easeOutCubic)
             .setOnComplete(() => inventoryPanel.gameObject.SetActive(false));
+
+        if (hideItem) HideItemInfo();
     }
 
     public void ShowInventory()
